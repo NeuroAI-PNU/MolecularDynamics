5efa8f29f3bdbfdfa278134227fe8da5b04a02a5
Deokhyeon Yoon <152151958+DeokhyeonYoon@users.noreply.github.com>
2024-10-26 16:42:02 +0900

    Add files via upload

---
+++ MD simulation/20240822_rank_50ns_PME_tmux_4_final.py
@@ -0,0 +1,315 @@
+import copy
+from pathlib import Path
+import sys
+
+import requests
+from IPython.display import display
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+import mdtraj as md
+import pdbfixer
+import openmm as mm
+import openmm.app as app
+from openmm import unit
+from openff.toolkit.topology import Molecule, Topology
+from openmmforcefields.generators import GAFFTemplateGenerator
+import warnings
+
+def prepare_protein(
+    pdb_file, ignore_missing_residues=True, ignore_terminal_missing_residues=True, ph=7.0
+):
+    """
+    Use pdbfixer to prepare the protein from a PDB file. Hetero atoms such as ligands are
+    removed and non-standard residues replaced. Missing atoms to existing residues are added.
+    Missing residues are ignored by default, but can be included.
+
+    Parameters
+    ----------
+    pdb_file: pathlib.Path or str
+        PDB file containing the system to simulate.
+    ignore_missing_residues: bool, optional
+        If missing residues should be ignored or built.
+    ignore_terminal_missing_residues: bool, optional
+        If missing residues at the beginning and the end of a chain should be ignored or built.
+    ph: float, optional
+        pH value used to determine protonation state of residues
+
+    Returns
+    -------
+    fixer: pdbfixer.pdbfixer.PDBFixer
+        Prepared protein system.
+    """
+    fixer = pdbfixer.PDBFixer(str(pdb_file))
+    fixer.removeHeterogens()  # co-crystallized ligands are unknown to PDBFixer
+    fixer.findMissingResidues()  # identify missing residues, needed for identification of missing atoms
+
+    # if missing terminal residues shall be ignored, remove them from the dictionary
+    if ignore_terminal_missing_residues:
+        chains = list(fixer.topology.chains())
+        keys = fixer.missingResidues.keys()
+        for key in list(keys):
+            chain = chains[key[0]]
+            if key[1] == 0 or key[1] == len(list(chain.residues())):
+                del fixer.missingResidues[key]
+
+    # if all missing residues shall be ignored ignored, clear the dictionary
+    if ignore_missing_residues:
+        fixer.missingResidues = {}
+
+    fixer.findNonstandardResidues()  # find non-standard residue
+    fixer.replaceNonstandardResidues()  # replace non-standard residues with standard one
+    fixer.findMissingAtoms()  # find missing heavy atoms
+    fixer.addMissingAtoms()  # add missing atoms and residues
+    fixer.addMissingHydrogens(ph)  # add missing hydrogens
+    return fixer
+
+def prepare_ligand(pdb_file, resname, smiles, depict=True):
+    """
+    Prepare a ligand from a PDB file via adding hydrogens and assigning bond orders. A depiction
+    of the ligand before and after preparation is rendered in 2D to allow an inspection of the
+    results. Huge thanks to @j-wags for the suggestion.
+
+    Parameters
+    ----------
+    pdb_file: pathlib.PosixPath
+       PDB file containing the ligand of interest.
+    resname: str
+        Three character residue name of the ligand.
+    smiles : str
+        SMILES string of the ligand informing about correct protonation and bond orders.
+    depict: bool, optional
+        show a 2D representation of the ligand
+
+    Returns
+    -------
+    prepared_ligand: rdkit.Chem.rdchem.Mol
+        Prepared ligand.
+    """
+    # split molecule
+    rdkit_mol = Chem.MolFromPDBFile(str(pdb_file), proximityBonding=True, sanitize=False)
+    print(rdkit_mol)
+    rdkit_mol_split = Chem.rdmolops.SplitMolByPDBResidues(rdkit_mol)
+
+    # extract the ligand and remove any already present hydrogens
+    ligand = rdkit_mol_split[resname]
+    ligand = Chem.RemoveHs(ligand)
+
+    # assign bond orders from template
+    reference_mol = Chem.MolFromSmiles(smiles)
+    prepared_ligand = AllChem.AssignBondOrdersFromTemplate(reference_mol, ligand)
+    prepared_ligand.AddConformer(ligand.GetConformer(0))
+
+    # protonate ligand
+    prepared_ligand = Chem.rdmolops.AddHs(prepared_ligand, addCoords=True)
+    prepared_ligand = Chem.MolFromMolBlock(Chem.MolToMolBlock(prepared_ligand))
+
+    # 2D depiction
+    if depict:
+        ligand_2d = copy.deepcopy(ligand)
+        prepared_ligand_2d = copy.deepcopy(prepared_ligand)
+        AllChem.Compute2DCoords(ligand_2d)
+        AllChem.Compute2DCoords(prepared_ligand_2d)
+        display(
+            Draw.MolsToGridImage(
+                [ligand_2d, prepared_ligand_2d], molsPerRow=2, legends=["original", "prepared"]
+            )
+        )
+
+    # return ligand
+    return prepared_ligand
+
+def rdkit_to_openmm(rdkit_mol, name="LIG"):
+    """
+    Convert an RDKit molecule to an OpenMM molecule.
+    Inspired by @hannahbrucemcdonald and @glass-w.
+
+    Parameters
+    ----------
+    rdkit_mol: rdkit.Chem.rdchem.Mol
+        RDKit molecule to convert.
+    name: str
+        Molecule name.
+
+    Returns
+    -------
+    omm_molecule: openmm.app.Modeller
+        OpenMM modeller object holding the molecule of interest.
+    """
+    # convert RDKit to OpenFF
+    off_mol = Molecule.from_rdkit(rdkit_mol)
+    #print(off_mol)
+
+    # add name for molecule
+    off_mol.name = name
+    #print(off_mol.name)
+
+    # add names for atoms
+    element_counter_dict = {}
+    for off_atom, rdkit_atom in zip(off_mol.atoms, rdkit_mol.GetAtoms()):
+        element = rdkit_atom.GetSymbol()
+        if element in element_counter_dict.keys():
+            element_counter_dict[element] += 1
+        else:
+            element_counter_dict[element] = 1
+        off_atom.name = element + str(element_counter_dict[element])
+    #print(element_counter_dict)
+
+    # convert from OpenFF to OpenMM
+    off_mol_topology = off_mol.to_topology()
+    mol_topology = off_mol_topology.to_openmm()
+    #print(mol_topology)
+    mol_positions = off_mol.conformers[0]
+    #print(mol_positions)
+
+    # convert units from Ångström to nanometers
+    # since OpenMM works in nm
+    mol_positions = mol_positions.to("nanometers")
+    #print(mol_positions)
+
+    # combine topology and positions in modeller object
+    omm_mol = app.Modeller(mol_topology, mol_positions)
+
+    return omm_mol
+
+def merge_protein_and_ligand(protein, ligand):
+    """
+    Merge two OpenMM objects.
+
+    Parameters
+    ----------
+    protein: pdbfixer.pdbfixer.PDBFixer
+        Protein to merge.
+    ligand: openmm.app.Modeller
+        Ligand to merge.
+
+    Returns
+    -------
+    complex_topology: openmm.app.topology.Topology
+        The merged topology.
+    complex_positions: openmm.unit.quantity.Quantity
+        The merged positions.
+    """
+    # combine topologies
+    md_protein_topology = md.Topology.from_openmm(protein.topology)  # using mdtraj for protein top
+    #print(md_protein_topology)
+    md_ligand_topology = md.Topology.from_openmm(ligand.topology)  # using mdtraj for ligand top
+    #print(md_ligand_topology)
+    md_complex_topology = md_protein_topology.join(md_ligand_topology)  # add them together
+    #print(md_complex_topology)
+    complex_topology = md_complex_topology.to_openmm()
+    #print(complex_topology)
+
+    # combine positions
+    total_atoms = len(protein.positions) + len(ligand.positions)
+
+    # create an array for storing all atom positions as tupels containing a value and a unit
+    # called OpenMM Quantities
+    complex_positions = unit.Quantity(np.zeros([total_atoms, 3]), unit=unit.nanometers)
+    complex_positions[: len(protein.positions)] = protein.positions  # add protein positions
+    complex_positions[len(protein.positions) :] = ligand.positions  # add ligand positions
+
+    return complex_topology, complex_positions
+
+def generate_forcefield(
+    rdkit_mol=None, protein_ff="amber14-all.xml", solvent_ff="amber14/tip3pfb.xml"
+):
+    """
+    Generate an OpenMM Forcefield object and register a small molecule.
+
+    Parameters
+    ----------
+    rdkit_mol: rdkit.Chem.rdchem.Mol
+        Small molecule to register in the force field.
+    protein_ff: string
+        Name of the force field.
+    solvent_ff: string
+        Name of the solvent force field.
+
+    Returns
+    -------
+    forcefield: openmm.app.Forcefield
+        Forcefield with registered small molecule.
+    """
+    forcefield = app.ForceField(protein_ff, solvent_ff)
+
+    if rdkit_mol is not None:
+        gaff = GAFFTemplateGenerator(
+            molecules=Molecule.from_rdkit(rdkit_mol, allow_undefined_stereo=True)
+        )
+        forcefield.registerTemplateGenerator(gaff.generator)
+
+    return forcefield
+
+if __name__=="__main__":
+    warnings.filterwarnings("ignore")
+
+    # prepare protein
+    prepare_protein = prepare_protein("./rank5.pdb", ignore_missing_residues=False)
+
+    # prepare ligand
+    pdb_path = "./rank5.pdb"
+    ligand_name = "UNL"
+    smiles = "COc1ccccc1NC(=O)N1CCN(c2ccccc2)CC1"
+
+    rdkit_ligand = prepare_ligand(pdb_path, ligand_name, smiles)
+
+    # rdkit object to openmm object
+    omm_ligand = rdkit_to_openmm(rdkit_ligand, ligand_name)
+
+    # merge protein and ligand
+    complex_topology, complex_positions = merge_protein_and_ligand(prepare_protein, omm_ligand)
+    print("Complex topology has", complex_topology.getNumAtoms(), "atoms.")
+
+    # generate forcefield
+    forcefield = generate_forcefield(rdkit_ligand)
+
+    modeller = app.Modeller(complex_topology, complex_positions)
+    modeller.addSolvent(forcefield, padding=1.5 * unit.nanometers, ionicStrength=0.15 * unit.molar)
+
+    platform = mm.Platform.getPlatformByName("CUDA")
+    properties = {"DeviceIndex": "1", "Precision": "single"}
+
+    system = forcefield.createSystem(modeller.topology, nonbondedMethod=app.PME, nonbondedCutoff=1.0 * unit.nanometers, constraints=app.HBonds)
+    integrator = mm.LangevinIntegrator(309.65 * unit.kelvin, 1.0 / unit.picoseconds, 2.0 * unit.femtoseconds)
+    simulation = app.Simulation(modeller.topology, system, integrator, platform, properties)
+    simulation.context.setPositions(modeller.positions)
+
+    simulation.minimizeEnergy()
+    with open("240822_Final_topology_50ns_PME_5.pdb", "w") as pdb_file:
+        app.PDBFile.writeFile(
+            simulation.topology,
+            simulation.context.getState(getPositions=True, enforcePeriodicBox=True).getPositions(),
+            file=pdb_file,
+            keepIds=True,
+        )
+
+    steps = 25000000
+    write_interval = 500
+    log_interval = 50000
+    simulation.reporters.append(
+        md.reporters.XTCReporter(file=str("240822_Final_trajectory_50ns_PME_5.xtc"), reportInterval=write_interval)
+    )
+    simulation.reporters.append(
+        app.StateDataReporter(
+            sys.stdout,
+            log_interval,
+            step=True,
+            potentialEnergy=True,
+            temperature=True,
+            progress=True,
+            remainingTime=True,
+            speed=True,
+            totalSteps=steps,
+            separator="\t",
+            )
+        )
+
+    simulation.context.setVelocitiesToTemperature(309.65 * unit.kelvin)
+    simulation.step(steps)
+
+    import os
+    result = "./240822_Final_trajectory_50ns_PME_5.xtc"
+    file_info = os.stat(result)
+    print(file_info)
